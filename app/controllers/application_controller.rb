3078814e331ff12fbe213cd6fe96aabdfad5d64f
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-08-16 21:36:14 +0900

    [Finish] 5-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	#
+	#def before_sign_in_path_for(resource)
+     	#toppage_path
+  	#end
+	 def after_sign_in_path_for(resource)
+		mypage_path (resource)
+	end
+	 def after_sign_out_path_for(resource)
+		root_path
+	end
+	
 		protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
+  #root 'homes#top', as: :toppage
+  root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
+  get "users/:id" => "users#show", as: :mypage
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
 end
