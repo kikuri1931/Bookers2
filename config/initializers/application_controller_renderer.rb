893f2dbfd0cc39ac745b029c4cc763af4e28c634
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-08-17 23:44:33 +0900

    [Finish] 5-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
+Refile.secret_key = '5db07b618bdd67f72d80c81944757b676c465061b8dc0a226186702c2dfdf6c93bc74190a274c5b0bf523735a598aaf078e6856f4ed5e263ba20c32c7212b87c'
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
   get "users/:id" => "users#show", as: :mypage
+  post "books/:id" => "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update, :create]
+  
+  resources :books, only: [:new, :create, :show]
 end
